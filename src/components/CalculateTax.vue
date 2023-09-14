23eb8d34e4b51ef57f9c5251cb1eced7267c2b28
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-09-14 21:40:07 +0900

    入力項目の作成

--- src/components/CalculateTax.vue
+++ src/components/CalculateTax.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        
+        <p>これはCalculateTaxコンポーネントです</p>
+
+        <div>
+            <input type="text" placeholder="品目" >
+            <input type="number" placeholder="数量">
+            <input type="number" placeholder="合計額">
+        </div>
     </div>
 </template>
+
+<script>
+
+</script>
