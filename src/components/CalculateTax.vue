367febeabcd695d718b9f52b354547015bb83179
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-09-14 23:08:55 +0900

    無駄なv-forを削除

--- src/components/CalculateTax.vue
+++ src/components/CalculateTax.vue
@@ -7,7 +7,9 @@
             <button v-if="isMoreZero" @click="removeRow">列の削除</button>
             <div v-for="i of rowCount" :key="i">
                 <div class="input-row">
-                    <div v-html="inputRow" v-for="inputRow of inputRows" :key="inputRow" ></div>
+                    <input type="text" placeholder="品目" >
+                    <input type="number" placeholder="数量">
+                    <input type="number" placeholder="合計額">
                 </div>
             </div>
         </div>
@@ -19,11 +21,6 @@ export default {
     data() {
         return {
             rowCount: 3,
-            inputRows: [
-                '<input type="text" placeholder="品目" >',
-                '<input type="number" placeholder="数量">',
-                '<input type="number" placeholder="合計額">',
-            ],
         };
     },
     methods: {
