5abd3a228835c96b5025eb7e8b69c6ec2628ab4f
KazumaEnomoto <kk.yzgb1310@gmail.com>
2023-09-16 00:02:58 +0900

    addTax関数の名前を変更、税込価格計算、小数点以下切り捨て処理を関数として切り出し

--- src/components/CalculateTax.vue
+++ src/components/CalculateTax.vue
@@ -18,13 +18,13 @@
             </div>
             <div>
                 <p>インプットの合計額：{{sumValues}}</p>
-                <p>インプットに税率をかけた合計額：{{taxAddedValues}}</p>
-                <p>実際に算出される合計額：{{Math.floor(taxAddedValues)}}</p>
+                <p>インプットに税率をかけた合計額：{{addTaxSumValues}}</p>
+                <p>実際に算出される合計額：{{Math.floor(addTaxSumValues)}}</p>
                 <p>レシートの合計額</p>
                 <input type="number" v-model.number="inputSum">
             </div>
         </div>
-        <button @click="addTax()">税込価格を計算する</button>
+        <button @click="calculateTaxedValues()">税込価格を計算する</button>
         <button @click="checkValue()">計算する</button>
         <button @click="check()">確認する</button>
     </div>
@@ -66,16 +66,16 @@ export default {
             }
         },
         checkValue() {
-            if (Math.floor(this.taxAddedValues) === this.inputSum) {
+            if (Math.floor(this.addTaxSumValues) === this.inputSum) {
                 console.log("合計値と合致しています");
             } else {
                 console.log("合計値と合致していません");
             }
         },
-        addTax() {
+        calculateTaxedValues() {
             this.taxedValues = [];
             this.values.forEach((value) => {
-                this.taxedValues.push(value * (1 + this.taxRate / 100));
+                this.taxedValues.push(this.roundDown(this.addTax(value)));
             });
         },
         check() {
@@ -84,6 +84,12 @@ export default {
             console.log(this.values);
             console.log(this.taxedValues);
         },
+        addTax(e) {
+            return e * (1 + this.taxRate / 100);
+        },
+        roundDown(e) {
+            return Math.floor(e * 100) / 100; // 入力値の小数点第３位以下を切り捨て
+        }
     },
     computed: {
         // 入力列削除ボタンの表示切り替え基準
@@ -96,10 +102,12 @@ export default {
             }, 0);
             return sumValue;
         },
-        taxAddedValues() {
+        addTaxSumValues() {
             // 少数内における微妙な誤差の除去
-            return Math.floor(this.sumValues * (this.taxRate / 100 + 1)  * 100) / 100; 
-        }
+            return this.roundDown(this.addTax(this.sumValues));
+        },
+        
+        
     }
 }
 </script>
